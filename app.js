ffe77545e1d693779f1a35c3ed9fa1bcffeb2b23
haerang <gofkdvjvl@naver.com>
2020-03-06 23:56:16 +0900

    form action with post method, url test

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  //   console.log(req.url, req.method, req.headers);
   //   process.exit();
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
